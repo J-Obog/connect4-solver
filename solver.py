27e9300bf9a0ed36ee990011ac60efcc994f44ce
J-Obog <jobogbaimhe@gmail.com>
2025-08-02 20:14:16 -0400

    adding helper to get legal moves

--- solver.py
+++ solver.py
@@ -6,6 +6,7 @@ START_STATE = "-"
 s_map = {}
 
 COL_TO_IDX = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6}
+IDX_TO_COL = {0:"A", 1:"B", 2:"C", 3:"D", 4:"E", 5:"F", 6:"G"}
 
 class GameState:
     def __init__(self, board_pgn):
@@ -27,6 +28,18 @@ class GameState:
         self.board[row][col] = color
 
     def legal_moves(self, color):
+        moves = []
+        for col in range(7):
+            i = 0
+            while (i < 6) and (self.board[i][col] != ""):
+                i += 1
+
+            if i < 6:
+                moves.append(f"{color.lower()}{IDX_TO_COL[col]}{i+1}")
+
+        return moves  
+
+    def winner(self):
         pass
 
 
